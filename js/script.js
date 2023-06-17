409dac8f8df5ea28236252f098fe452129f39097
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-06-17 19:59:31 +0300

    Added animation by scroll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-
+window.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".resume__block.hidden");
+  const skillsBlock = document.querySelectorAll(".skills__item.hidden");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((element) => {
+      if (!element.isIntersecting) return;
+      element.target.classList.add("show");
+    });
+  });
+  sections.forEach((el) => observer.observe(el));
+  skillsBlock.forEach((el) => observer.observe(el));
+});
