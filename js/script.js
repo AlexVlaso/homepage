1a96f7337a8e73775fec440e34198b65b62295f0
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-06-18 11:45:19 +0300

    Added timeline section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // Animation by scroll
+
   const sections = document.querySelectorAll(".resume__block.hidden");
   const skillsBlock = document.querySelectorAll(".skills__item.hidden");
   const observer = new IntersectionObserver((entries) => {
@@ -9,4 +11,26 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   sections.forEach((el) => observer.observe(el));
   skillsBlock.forEach((el) => observer.observe(el));
+
+  // Timeline tabs
+  const labels = document.querySelectorAll(".timeline__label");
+  const contents = document.querySelectorAll(".timeline__content");
+  const setActiveLabel = (label) => {
+    labels.forEach((el) => {
+      el.classList.remove("timeline__label_active");
+    });
+    label.classList.add("timeline__label_active");
+  };
+  const setActiveContent = (index) => {
+    contents.forEach((el) => {
+      el.classList.remove("timeline__content_active");
+    });
+    contents[index].classList.add("timeline__content_active");
+  };
+  labels.forEach((label, i) => {
+    label.addEventListener("click", () => {
+      setActiveLabel(label);
+      setActiveContent(i);
+    });
+  });
 });
